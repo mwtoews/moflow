dcca1443e525c5654e4c318337205535991ab838
mwtoews <mwtoews@gmail.com>
2014-05-07 01:39:33 +1200

    Several enhancements to read either DIS or DISU packages, start BAS6 and RCH
    Several improvements:
     - Replace MFReaderError with regular exceptions, and isolate exceptions
       in a try block that preserves the trace, and shows the position in
       the file
     - Use logger for showing messages
     - Start of HDF5 support from GMS, plenty more work needed
     - Add ASP package reference
     - Add reference to GMS 10 tutorials to reading tests

--- tests/test_mf.py
+++ tests/test_mf.py
@@ -13,14 +13,14 @@ except ImportError:
     sys.path.append('.')
     from moflow import mf
 
-#import logging
+import logging
 #mf.logger.level = logging.DEBUG
 #mf.logger.level = logging.INFO
-#mf.logger.level = logging.WARN
+mf.logger.level = logging.WARN
 #mf.logger.level = logging.ERROR
 
 
-class TEST(mf._MFPackage):
+class TestPackage(mf._MFPackage):
     par1 = None
     par2 = None
 
@@ -28,8 +28,8 @@ class TEST(mf._MFPackage):
 class TestMFPackage(unittest.TestCase):
 
     def test_mf_reader_basic(self):
-        p = TEST()
-        p.fpath = BytesIO(dedent('''\
+        p = TestPackage()
+        f = BytesIO(dedent('''\
             # A comment
             100 ignore
             200 -2.4E-12
@@ -37,30 +37,30 @@ class TestMFPackage(unittest.TestCase):
             -44 888.0
             last line
         '''))
-        r = mf._MFFileReader(p)
+        r = mf._MFFileReader(f, p)
         self.assertTrue(r.not_eof)
         self.assertEqual(r.lineno, 0)
         self.assertEqual(len(r), 6)
-        # Data Set 0: Text
+        # 0: Text
         r.read_text()
         self.assertEqual(r.lineno, 1)
         self.assertEqual(p.text, ['A comment'])
-        # Data Set 1: an item
+        # 1: an item
         self.assertEqual(r.get_items(1, 1, 'i'), [100])
         self.assertEqual(r.lineno, 2)
-        # Data Set 2: two named items
-        self.assertRaises(mf.MFReaderError, r.read_named_items,
+        # 2: two named items
+        self.assertRaises(ValueError, r.read_named_items,
                           2, ('par1', 'par2'), 'i')
         r.lineno -= 1  # manually scroll back 1 line and try again
-        r.read_named_items(2, ('par1', 'par2'), ['i', 'f'])
-        self.assertEqual(p.par1, 200)
+        r.read_named_items(2, ('par1', 'par2'), 'f')
+        self.assertEqual(p.par1, 200.0)
         self.assertAlmostEqual(p.par2, -2.4E-12)
-        # Data Set 3: three named items
-        items = r.get_named_items(3, ['a', 'b', 'c'], ['f', 'i', 's'])
-        self.assertEqual(items, {'a': 4.0, 'b': 500, 'c': 'FREE'})
-        # Data Set 4: two named items
-        r.read_named_items(4, ['par1', 'par2'], ['i', 'f'])
-        self.assertEqual(p.par1, -44)
+        # 3: three named items
+        items = r.get_named_items(3, ['a', 'b'], 'f')
+        self.assertEqual(items, {'a': 4.0, 'b': 500.0})
+        # 4: two named items
+        r.read_named_items(4, ['par1', 'par2'], 'f')
+        self.assertEqual(p.par1, -44.0)
         self.assertAlmostEqual(p.par2, 888.0)
         # post-Data Set
         self.assertTrue(r.not_eof)
@@ -68,13 +68,13 @@ class TestMFPackage(unittest.TestCase):
         self.assertEqual(r.lineno, 6)
         self.assertFalse(r.not_eof)
         # Try to read past EOF
-        self.assertRaises(mf.MFReaderError, r.next_line)
+        self.assertRaises(IndexError, r.next_line)
         self.assertEqual(r.lineno, 6)
 
     def test_mf_reader_empty(self):
-        p = TEST()
-        p.fpath = BytesIO(dedent('''# Empty file'''))
-        r = mf._MFFileReader(p)
+        p = TestPackage()
+        f = BytesIO(dedent('''# Empty file'''))
+        r = mf._MFFileReader(f, p)
         self.assertTrue(r.not_eof)
         self.assertEqual(r.lineno, 0)
         self.assertEqual(len(r), 1)
@@ -87,9 +87,9 @@ class TestMFPackage(unittest.TestCase):
     def test_mf_read_free_arrays(self):
         # Examples from page 8-59 of TM6A16_MF2005
         m = mf.Modflow()
-        p = TEST()
+        p = TestPackage()
         m.append(p)
-        p.fpath = BytesIO(dedent('''\
+        f = BytesIO(dedent('''\
         CONSTANT  5.7     This sets an entire array to the value "5.7".
         INTERNAL  1.0  (7F4.0)  3    This reads the array values from the ...
          1.2 3.7 9.3 4.2 2.2 9.9 1.0
@@ -115,11 +115,11 @@ class TestMFPackage(unittest.TestCase):
              [4.5, 5.7, 2.2, 1.1, 1.7, 6.7, 6.9],
              [7.4, 3.5, 7.8, 8.5, 7.4, 6.8, 8.8]], 'f')
         m[47] = BytesIO(d2_expected.tostring())
-        r = mf._MFFileReader(p)
+        r = mf._MFFileReader(f, p)
         # Data Number 1: Read constant 4x5 array
         d1_shape = (4, 7)
         d1_expected = np.ones(d1_shape, 'f') * 5.7
-        d1 = r.get_array(1, d1_shape, 'f')
+        d1 = r.get_array(1, d1_shape, 'f', return_dict=True)
         self.assertFalse(hasattr(d1, 'locat'))
         self.assertEqual(d1['cntrl'], 'CONSTANT')
         self.assertEqual(d1['cnstnt'], '5.7')
@@ -130,7 +130,7 @@ class TestMFPackage(unittest.TestCase):
         self.assertEqual(r.lineno, 1)
         # Data Number 2: Read internal 4x7 array
         d2_shape = (4, 7)
-        d2 = r.get_array(2, d2_shape, 'f')
+        d2 = r.get_array(2, d2_shape, 'f', return_dict=True)
         self.assertEqual(d2['cntrl'], 'INTERNAL')
         self.assertEqual(d2['cnstnt'], '1.0')
         self.assertEqual(d2['fmtin'], '(7F4.0)')
@@ -141,7 +141,7 @@ class TestMFPackage(unittest.TestCase):
         self.assertTrue((d2['array'] == d2_expected).all())
         self.assertEqual(r.lineno, 6)
         # Data Number 3: EXTERNAL ASCII
-        d3 = r.get_array(3, d2_shape, 'f')
+        d3 = r.get_array(3, d2_shape, 'f', return_dict=True)
         self.assertEqual(d3['cntrl'], 'EXTERNAL')
         self.assertEqual(d3['nunit'], 52)
         self.assertEqual(d3['cnstnt'], '1.0')
@@ -151,7 +151,7 @@ class TestMFPackage(unittest.TestCase):
         self.assertTrue((d3['array'] == d2_expected).all())
         self.assertEqual(r.lineno, 7)
         # Data Number 4: EXTERNAL BINARY
-        d4 = r.get_array(4, d2_shape, 'f')
+        d4 = r.get_array(4, d2_shape, 'f', return_dict=True)
         self.assertEqual(d4['cntrl'], 'EXTERNAL')
         self.assertEqual(d4['nunit'], 47)
         self.assertEqual(d4['cnstnt'], '1.0')
@@ -164,7 +164,7 @@ class TestMFPackage(unittest.TestCase):
         d5_fname = 'test.dat'
         with open(d5_fname, 'w') as fp:
             fp.write(d2_str)
-        d5 = r.get_array(5, d2_shape, 'f')
+        d5 = r.get_array(5, d2_shape, 'f', return_dict=True)
         os.unlink(d5_fname)
         self.assertEqual(d5['cntrl'], 'OPEN/CLOSE')
         self.assertEqual(d5['fname'], d5_fname)
@@ -178,10 +178,10 @@ class TestMFPackage(unittest.TestCase):
 
     def test_mf_read_fixed_arrays(self):
         m = mf.Modflow()
-        p = TEST()
+        p = TestPackage()
         p.nunit = 11
         m.append(p)
-        p.fpath = BytesIO('''\
+        f = BytesIO('''\
         11        1. (15f4.0)                    7  WETDRY-1
   2.  2.  2.  2.  2.  2.  2.  2. -2. -2. -2. -2. -2. -2. -2.
   2.  2.  2.  2.  2.  2.  2.  2. -2. -2. -2. -2. -2. -2. -2.
@@ -202,6 +202,7 @@ class TestMFPackage(unittest.TestCase):
        -17        1. (binary)                    7
         16         1(24I3)                       3
         16         1(24I3)                       3
+         0      145.
 ''')
         # Prepare two ASCII data sets for unit 16
         m[16] = BytesIO('''\
@@ -247,13 +248,13 @@ class TestMFPackage(unittest.TestCase):
              [4.5, 5.7, 2.2, 1.1, 1.7, 6.7, 6.9],
              [7.4, 3.5, 7.8, 8.5, 7.4, 6.8, 8.8]], 'f')
         m[17] = BytesIO(d6_expected.tostring())
-        r = mf._MFFileReader(p)
+        r = mf._MFFileReader(f, p)
         # Data Number 1
         d1_shape = (7, 15)
         a = [[2.] * 8 + [-2.] * 7]
         b = [[2.] * 3 + [-2.] + [2.] * 4 + [-2.] * 7]
         d1_expected = np.array(a * 2 + b + a * 2 + b + a)
-        d1 = r.get_array(1, d1_shape, 'f')
+        d1 = r.get_array(1, d1_shape, 'f', return_dict=True)
         self.assertFalse(hasattr(d1, 'cntrl'))
         self.assertEqual(d1['locat'], 11)
         self.assertEqual(d1['cnstnt'], '1.')
@@ -267,7 +268,7 @@ class TestMFPackage(unittest.TestCase):
         d2_shape = (2, 12)
         d2_expected = np.ones(d2_shape)
         d2_expected[1, 2] = 9
-        d2 = r.get_array(2, d2_shape, 'i')
+        d2 = r.get_array(2, d2_shape, 'i', return_dict=True)
         self.assertEqual(d2['locat'], 11)
         self.assertEqual(d2['cnstnt'], '1')
         self.assertEqual(d2['fmtin'], '(12I2)')
@@ -281,7 +282,7 @@ class TestMFPackage(unittest.TestCase):
         d3_expected = np.array(2 * [[-1] + [1] * 11 + [-1]], 'i')
         d3a = np.empty(d3_shape, 'i')
         for ilay in range(d3_shape[0]):
-            d3 = r.get_array(3, d3_shape[1], 'i')
+            d3 = r.get_array(3, d3_shape[1], 'i', return_dict=True)
             self.assertEqual(d3['locat'], 11)
             self.assertEqual(d3['fmtin'], '(13I3)')
             self.assertEqual(d3['iprn'], '')
@@ -294,7 +295,7 @@ class TestMFPackage(unittest.TestCase):
         # Data Number 4
         d4_shape = (4, 8)
         d4_expected = np.ones(d4_shape, 'f') * 6
-        d4 = r.get_array(4, d4_shape, 'f')
+        d4 = r.get_array(4, d4_shape, 'f', return_dict=True)
         self.assertEqual(d4['locat'], 0)
         self.assertEqual(d4['cnstnt'], '6.')
         self.assertEqual(d4['fmtin'], '(15F4.0)')
@@ -306,7 +307,7 @@ class TestMFPackage(unittest.TestCase):
         # Data Number 5
         d5_shape = (7, 6)
         d5_expected = np.ones(d5_shape) * 8
-        d5 = r.get_array(5, d5_shape, 'i')
+        d5 = r.get_array(5, d5_shape, 'i', return_dict=True)
         self.assertEqual(d5['locat'], 0)
         self.assertEqual(d5['cnstnt'], '8')
         self.assertEqual(d5['fmtin'], '(12I2)')
@@ -317,7 +318,7 @@ class TestMFPackage(unittest.TestCase):
         self.assertEqual(r.lineno, 17)
         # Data Number 6
         d6_shape = (4, 7)
-        d6 = r.get_array(5, d6_shape, 'f')
+        d6 = r.get_array(6, d6_shape, 'f', return_dict=True)
         self.assertEqual(d6['locat'], -17)
         self.assertEqual(d6['cnstnt'], '1.')
         self.assertEqual(d6['fmtin'], '(BINARY)')
@@ -333,7 +334,7 @@ class TestMFPackage(unittest.TestCase):
         d7_expected[1, 7:10, 7:10] = 1
         d7a = np.empty(d7_shape, 'i')
         for ilay in range(d7_shape[0]):
-            d7 = r.get_array(3, d7_shape[1:], 'i')
+            d7 = r.get_array(7, d7_shape[1:], 'i', return_dict=True)
             self.assertEqual(d7['locat'], 16)
             self.assertEqual(d7['fmtin'], '(24I3)')
             self.assertEqual(d7['iprn'], '3')
@@ -342,6 +343,17 @@ class TestMFPackage(unittest.TestCase):
         self.assertEqual(d7a.shape, d7_expected.shape)
         self.assertTrue((d7a == d7_expected).all())
         self.assertEqual(r.lineno, 20)
+        # Data Number 8
+        d8_shape = (2, 3)
+        d8_expected = 145.0 * np.ones(d8_shape, 'f')
+        d8 = r.get_array(8, d8_shape, 'f', return_dict=True)
+        self.assertEqual(d8['locat'], 0)
+        self.assertEqual(d8['cnstnt'], '145.')
+        self.assertFalse(hasattr(d8, 'fmtin'))
+        self.assertFalse(hasattr(d8, 'iprn'))
+        self.assertEqual(d8['array'].shape, d8_expected.shape)
+        self.assertTrue((d8['array'] == d8_expected).all())
+        self.assertEqual(r.lineno, 21)
         self.assertFalse(r.not_eof)
 
 
@@ -349,6 +361,7 @@ mf2kdir = '../MODFLOW-2000/data'
 mf2005kdir = '../MODFLOW-2005/test-run'
 mfnwtdir = '../MODFLOW-NWT/data'
 mfusgdir = '../MODFLOW-USG/test'
+gmsdir = '/opt/aquaveo/gms/10.0'
 
 
 class TestMF(unittest.TestCase):
@@ -378,8 +391,9 @@ class TestMF(unittest.TestCase):
 
     @unittest.skipIf(not os.path.isdir(mf2kdir), 'could not find ' + mf2kdir)
     def test_modflow_2000(self):
-       # print(self.mf2kdir)
-        for nam in glob(os.path.join(self.mf2kdir, '*.nam')):
+        #print(mf2kdir)
+        for nam in glob(os.path.join(mf2kdir, '*.nam')):
+            #print(nam)
             m = mf.Modflow()
             m.read(nam)
             #print('%s: %s' % (os.path.basename(nam), ', '.join(list(m))))
@@ -387,33 +401,42 @@ class TestMF(unittest.TestCase):
     @unittest.skipIf(not os.path.isdir(mf2005kdir),
                      'could not find ' + mf2005kdir)
     def test_modflow_2005(self):
-        #print(self.mf2005kdir)
-        for nam in glob(os.path.join(self.mf2005kdir, '*.nam')):
+        #print(mf2005kdir)
+        for nam in glob(os.path.join(mf2005kdir, '*.nam')):
             m = mf.Modflow()
             m.read(nam)
             #print('%s: %s' % (os.path.basename(nam), ', '.join(list(m))))
 
     @unittest.skipIf(not os.path.isdir(mfnwtdir), 'could not find ' + mfnwtdir)
     def test_modflow_nwt(self):
-        #print(self.mfnwtdir)
-        for dirpath, dirnames, filenames in os.walk(self.mfnwtdir):
+        #print(mfnwtdir)
+        for dirpath, dirnames, filenames in os.walk(mfnwtdir):
             for nam in glob(os.path.join(dirpath, '*.nam')):
                 if 'SWI_data_files' in dirpath or 'SWR_data_files' in dirpath:
-                    ref_dir = dirpath
+                    ref_dir = dirpath  # normal
                 else:
-                    ref_dir = self.mfnwtdir
+                    ref_dir = mfnwtdir
                 m = mf.Modflow()
                 m.read(nam, ref_dir=ref_dir)
                 #print('%s: %s' % (os.path.basename(nam), ', '.join(list(m))))
 
     @unittest.skipIf(not os.path.isdir(mfusgdir), 'could not find ' + mfusgdir)
     def test_modflow_usg(self):
-        #print(self.mfusgdir)
-        for dirpath, dirnames, filenames in os.walk(self.mfusgdir):
+        # print(mfusgdir)
+        for dirpath, dirnames, filenames in os.walk(mfusgdir):
             for nam in glob(os.path.join(dirpath, '*.nam')):
                 m = mf.Modflow()
                 m.read(nam)
                 #print('%s: %s' % (os.path.basename(nam), ', '.join(list(m))))
+
+    @unittest.skipIf(not os.path.isdir(gmsdir), 'could not find ' + gmsdir)
+    def test_modflow_gms(self):
+        # print(gmsdir)
+        for dirpath, dirnames, filenames in os.walk(gmsdir):
+            for mfn in glob(os.path.join(dirpath, '*.mfn')):
+                m = mf.Modflow()
+                m.read(mfn)
+                #print('%s: %s' % (os.path.basename(mfn), ', '.join(list(m))))
 
 
 def test_suite():
